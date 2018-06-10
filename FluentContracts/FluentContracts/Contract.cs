2af62e5043c72ca25acd30d4b651d7652f83cf46
Todor Todorov <totollygeek@users.noreply.github.com>
2018-06-10 23:11:04 +0300

    Fixed some tabs

--- FluentContracts/FluentContracts/Contract.cs
+++ FluentContracts/FluentContracts/Contract.cs
@@ -3,14 +3,14 @@
 namespace FluentContracts
 {
 	public abstract class Contract<T>
-    {
-	    protected Contract(T argument, int lineNumber = 0, string filePath = "")
-	    {
-		    Argument = argument;
-		    CallerName = CallerNameLocator.GetNameOrDefault(filePath, lineNumber);
-	    }
+	{
+		protected Contract(T argument, int lineNumber = 0, string filePath = "")
+		{
+			Argument = argument;
+			CallerName = CallerNameLocator.GetNameOrDefault(filePath, lineNumber);
+		}
 
 		protected T Argument { get; }
-	    protected string CallerName { get; }
+		protected string CallerName { get; }
 	}
 }
