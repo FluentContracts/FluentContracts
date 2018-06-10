fde61313618baab2618b3f7e820dc55648a321c8
Todor Todorov <todor@todorov.bg>
2018-06-10 23:08:21 +0300

    Extended Guid contracts and started with some unit tests. Still just playing around :)

--- FluentContracts/FluentContracts.Tests/InfrastructureTests.cs
+++ FluentContracts/FluentContracts.Tests/InfrastructureTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using FluentAssertions;
+using FluentContracts.Exceptions;
 using FluentContracts.Primitives;
 using Xunit;
 
@@ -8,12 +9,13 @@ namespace FluentContracts.Tests
 	public class InfrastructureTests
     {
 	    [Fact]
+		[Trait("Infrastructure", null)]
 	    public void Test_Caller_Name_Discovery()
 	    {
 		    var someFancyGuid = Guid.Empty;
 
 		    Action failed = () => someFancyGuid.Must().NotBeEmpty();
-		    failed.Should().Throw<ArgumentOutOfRangeException>().WithMessage("Exception of type 'System.ArgumentOutOfRangeException' was thrown.\r\nParameter name:  someFancyGuid");
+		    failed.Should().Throw<InvalidArgumentValueException>().WithMessage("Exception of type 'FluentContracts.Exceptions.InvalidArgumentValueException' was thrown.\r\nParameter name:  someFancyGuid");
 	    }
     }
 }
--- FluentContracts/FluentContracts/Primitives/GuidContract.cs
+++ FluentContracts/FluentContracts/Primitives/GuidContract.cs
@@ -1,17 +1,43 @@
 ﻿using System;
+using System.Linq;
 
 namespace FluentContracts.Primitives
 {
 	public class GuidContract : Contract<Guid>
-    {
-	    public GuidContract(Guid argument, int lineNumber = 0, string filePath = "") 
-		    : base(argument, lineNumber, filePath)
-	    {
-	    }
+	{
+		public GuidContract(Guid argument, int lineNumber = 0, string filePath = "")
+			: base(argument, lineNumber, filePath)
+		{
+		}
 
-	    public void NotBeEmpty()
-	    {
-		    Validator.CheckOutOfRange(Argument != Guid.Empty, CallerName);
-	    }
-    }
+		public void NotBeEmpty()
+		{
+			Validator.CheckSpecificValue(Argument != Guid.Empty, CallerName);
+		}
+
+		public void BeEmpty()
+		{
+			Validator.CheckSpecificValue(Argument == Guid.Empty, CallerName);
+		}
+
+		public void Be(Guid expectedValue)
+		{
+			Validator.CheckSpecificValue(Argument == expectedValue, CallerName);
+		}
+
+		public void NotBe(Guid expectedValue)
+		{
+			Validator.CheckSpecificValue(Argument != expectedValue, CallerName);
+		}
+
+		public void BeAnyOf(params Guid[] expectedValues)
+		{
+			Validator.CheckOutOfRange(expectedValues.Any(g => g == Argument), CallerName);
+		}
+
+		public void NotBeAnyOf(params Guid[] expectedValues)
+		{
+			Validator.CheckOutOfRange(expectedValues.All(g => g != Argument), CallerName);
+		}
+	}
 }
