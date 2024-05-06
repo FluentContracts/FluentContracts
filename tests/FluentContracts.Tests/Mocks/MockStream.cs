9797967ab3e978e85c521b87b79121075d428a9a
Todor Todorov <totollygeek@users.noreply.github.com>
2024-05-06 17:43:48 +0300

    Add tests for Stream contracts

---
+++ tests/FluentContracts.Tests/Mocks/MockStream.cs
@@ -0,0 +1,47 @@
+using System.IO;
+
+namespace FluentContracts.Tests.Mocks;
+
+public class MockStream(
+    bool canRead = true, 
+    bool canSeek = true, 
+    bool canWrite = true, 
+    long length = 10, 
+    long position = 5, 
+    bool canTimeout = true) : Stream
+{   
+    public override void Flush()
+    {
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        return 0;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        return 0;
+    }
+
+    public override void SetLength(long value)
+    {
+        length = value;
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+    }
+
+    public override bool CanRead => canRead;
+    public override bool CanSeek => canSeek;
+    public override bool CanWrite => canWrite;
+    public override long Length => length;
+    public override long Position
+    {
+        get => position;
+        set => position = value;
+    }
+
+    public override bool CanTimeout => canTimeout;
+}
---
+++ tests/FluentContracts.Tests/Streams/StreamContractTests.cs
@@ -0,0 +1,198 @@
+using System;
+using System.IO;
+using FluentContracts.Contracts.Streams;
+using FluentContracts.Tests.Mocks;
+using FluentContracts.Tests.TestAttributes;
+using Xunit;
+
+namespace FluentContracts.Tests.Streams;
+
+[ContractTest("Stream")]
+public class StreamContractTests : Tests
+{
+    [Fact]
+    public void Test_Must_BeNull()
+    {
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            null,
+            new MockStream(),
+            (testArgument, message) => testArgument.Must().BeNull(message),
+            "testArgument");
+    }
+
+    [Fact]
+    public void Test_Must_NotBeNull()
+    {
+        TestContract<Stream, StreamContract<Stream>, ArgumentNullException>(
+            new MockStream(),
+            null,
+            (testArgument, message) => testArgument.Must().NotBeNull(message),
+            "testArgument");
+    }
+    
+    [Fact]
+    public void Test_Must_BeWriteable()
+    {
+        var success = new MockStream(canWrite: true);
+        var fail = new MockStream(canWrite: false);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().BeWriteable(message),
+            "testArgument");
+    }
+
+    [Fact]
+    public void Test_Must_NotBeWriteable()
+    {
+        var success = new MockStream(canWrite: false);
+        var fail = new MockStream(canWrite: true);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().NotBeWriteable(message),
+            "testArgument");
+    }
+    
+    [Fact]
+    public void Test_Must_BeReadable()
+    {
+        var success = new MockStream(canRead: true);
+        var fail = new MockStream(canRead: false);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().BeReadable(message),
+            "testArgument");
+    }
+
+    [Fact]
+    public void Test_Must_NotBeReadable()
+    {
+        var success = new MockStream(canRead: false);
+        var fail = new MockStream(canRead: true);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().NotBeReadable(message),
+            "testArgument");
+    }
+    
+    [Fact]
+    public void Test_Must_BeSeekable()
+    {
+        var success = new MockStream(canSeek: true);
+        var fail = new MockStream(canSeek: false);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().BeSeekable(message),
+            "testArgument");
+    }
+
+    [Fact]
+    public void Test_Must_NotBeSeekable()
+    {
+        var success = new MockStream(canSeek: false);
+        var fail = new MockStream(canSeek: true);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().NotBeSeekable(message),
+            "testArgument");
+    }
+    
+    [Fact]
+    public void Test_Must_BeAbleToTimeout()
+    {
+        var success = new MockStream(canTimeout: true);
+        var fail = new MockStream(canTimeout: false);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().BeAbleToTimeout(message),
+            "testArgument");
+    }
+
+    [Fact]
+    public void Test_Must_NotBeAbleToTimeout()
+    {
+        var success = new MockStream(canTimeout: false);
+        var fail = new MockStream(canTimeout: true);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().NotBeAbleToTimeout(message),
+            "testArgument");
+    }
+    
+    [Fact]
+    public void Test_Must_BeAtPosition()
+    {
+        var position = DummyData.GetLongPair();
+        
+        var success = new MockStream(position: position.TestArgument);
+        var fail = new MockStream(position: position.DifferentArgument);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().BeAtPosition(position.TestArgument, message),
+            "testArgument");
+    }
+
+    [Fact]
+    public void Test_Must_NotBeAtPosition()
+    {
+        var position = DummyData.GetLongPair();
+        
+        var success = new MockStream(position: position.DifferentArgument);
+        var fail = new MockStream(position: position.TestArgument);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().NotBeAtPosition(position.TestArgument, message),
+            "testArgument");
+    }
+    
+    
+    
+    [Fact]
+    public void Test_Must_BeWithLength()
+    {
+        var length = DummyData.GetLongPair();
+        
+        var success = new MockStream(length: length.TestArgument);
+        var fail = new MockStream(length: length.DifferentArgument);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().BeWithLength(length.TestArgument, message),
+            "testArgument");
+    }
+
+    [Fact]
+    public void Test_Must_NotBeWithLenght()
+    {
+        var length = DummyData.GetLongPair();
+        
+        var success = new MockStream(length: length.DifferentArgument);
+        var fail = new MockStream(length: length.TestArgument);
+        
+        TestContract<Stream, StreamContract<Stream>, ArgumentOutOfRangeException>(
+            success,
+            fail,
+            (testArgument, message) => testArgument.Must().NotBeWithLength(length.TestArgument, message),
+            "testArgument");
+    }
+}
